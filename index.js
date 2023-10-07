5aa90970f254a0304710c41c76cf978034dab30b
william lee <will.lee.npc@gmail.com>
2023-10-07 04:10:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",function(){
+    let newP = document.querySelector("#text");
+    newP.textContent = "This is really cool!";
+})
